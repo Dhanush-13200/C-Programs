3f61c1130513d6ba7c56574db23a14cad0ea4073
Dhanush-13200 <66692907+Dhanush-13200@users.noreply.github.com>
2020-06-25 11:54:50 +0530

    Create Enum.c

---
+++ Enum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**************
+This is used to 
+find the Element Stored.
+***************/
+
+int main()
+{
+    enum {A,B,C,D,E};
+    int x,y;
+    x = A;
+    y = D;
+    printf("The Value of A is %d\n", x);
+    printf("The Value of B is %d\n", y);
+    return 0;
+}
