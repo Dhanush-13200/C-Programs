9c0fa82d8eecfa60985d877558ef07b9cf654d09
Dhanush-13200 <66692907+Dhanush-13200@users.noreply.github.com>
2020-07-25 11:12:59 +0530

    Half Pyramid

---
+++ pattern5.c
@@ -0,0 +1,35 @@
+  #include <stdio.h>
+#include <stdlib.h>
+
+/*
+The number of rows N is passed as the input. The program prints the half pyramid
+using the numbers from 1 to N.
+
+Boundary Conditions:2 <= N <= 100
+
+Example Input/Output 1:
+Input:
+5
+Output:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
+int main()
+{
+    int x,i,j;
+    scanf("%d",&x);
+    for(i=0 ; i<x ; i++,printf("\n"))
+    {
+        int num=1;
+        for(j=0 ; j<=i ; j++)
+        {
+            printf("%d ",num++);
+
+        }
+    }
+    return 0;
+}  
