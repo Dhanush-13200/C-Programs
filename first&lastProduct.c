aaaa09c9db00fc7c5f87579c337b6f41a728fbaa
Dhanush-13200 <66692907+Dhanush-13200@users.noreply.github.com>
2020-08-08 22:04:24 +0530

    Create first&lastProduct.c

---
+++ first&lastProduct.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Input :
+514 2004
+
+Output :
+8
+Explanation :
+first variable last digit multiplied by second variable last digit
+
+*/
+
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    int a;
+    a=x%10;
+    while(y>9)
+    {
+        y/=10;
+    }
+    printf("%d",a*y);
+    return 0;
+}
